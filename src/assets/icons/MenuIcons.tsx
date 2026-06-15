29641371560c87df2c785c96b2291703acdfbb38
SonyNeto <sonyneto@gmail.com>
2026-06-14 21:03:44 -0300

    feat: add icons

---
+++ src/assets/icons/MenuIcons.tsx
@@ -0,0 +1,34 @@
+import type { SVGProps } from 'react';
+
+export function X(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} {...props}>
+      <path
+        fill="currentColor"
+        d="m 11,11 h 2 v 2 h -2 z m 2,0 h -2 z m 0,-2 h 2 v 2 h -2 z m 2,0 h -2 z m 0,-2 h 2 v 2 h -2 z m 2,0 h -2 z m 0,-2 h 2 v 2 h -2 z m 2,0 h -2 z m -6,8 h 2 v 2 h -2 z m 2,0 h -2 z m 0,2 h 2 v 2 h -2 z m 2,0 h -2 z m 0,2 h 2 v 2 h -2 z m 2,0 H 17 Z M 9,13 h 2 v 2 H 9 Z m 2,0 H 9 Z m -4,2 h 2 v 2 H 7 Z m 2,0 H 7 Z m -4,2 h 2 v 2 H 5 Z m 2,0 H 5 Z M 9,9 h 2 v 2 H 9 Z m 2,0 H 9 Z M 7,7 H 9 V 9 H 7 Z M 9,7 H 7 Z M 5,5 H 7 V 7 H 5 Z M 7,5 H 5 Z"
+      />
+    </svg>
+  );
+}
+
+export function TrashCan(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} {...props}>
+      <path
+        fill="currentColor"
+        d="M 10,22 C 0,0 0,0 10,22 Z m 0,0 C 0,0 0,0 10,22 Z m 0,0 C 0,0 0,0 10,22 Z m 2,0 h -2 z m -2,0 h 2 z m 2,0 h -2 z m 2,0 h -2 z m -2,0 h 2 z m 2,0 h -2 z m 2,0 C 0,0 0,0 16,22 Z m 0,0 C 0,0 0,0 16,22 Z m 0,0 C 0,0 0,0 16,22 Z m 0,0 h -2 z m -2,0 h 2 z m 2,0 H 14 Z M 6,18 H 4 Z m -2,0 h 2 z m 2,0 H 4 Z M 6,16 H 4 Z m -2,0 h 2 v 2 H 4 Z m 2,0 H 4 Z M 6,14 H 4 Z m -2,0 h 2 v 2 H 4 Z m 2,0 H 4 Z M 6,12 H 4 Z m -2,0 h 2 v 2 H 4 Z m 2,0 H 4 Z m 14,6 h -2 z m -2,0 h 2 z m 2,0 h -2 z m 0,-2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 0,-2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 0,-2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 H 18 Z M 6,10 H 4 Z m -2,0 h 2 v 2 H 4 Z m 2,0 H 4 Z M 6,8 H 4 Z M 4,8 h 2 v 2 H 4 Z M 6,8 H 4 Z m 14,2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 0,-2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 H 18 Z M 4,6 H 2 Z M 2,6 H 4 V 8 H 2 Z M 4,6 H 2 Z M 6,4 H 4 Z M 4,4 H 6 V 6 H 4 Z M 6,4 H 4 Z M 8,4 H 6 Z M 6,4 H 8 V 6 H 6 Z M 8,4 H 6 Z m 2,0 H 8 Z M 8,4 h 2 V 6 H 8 Z m 2,0 H 8 Z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 2,2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 H 20 Z M 12,2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 H 12 Z M 8,8 H 6 Z M 6,8 h 2 v 2 H 6 Z M 8,8 H 6 Z m 2,0 H 8 Z M 8,8 h 2 v 2 H 8 Z m 2,0 H 8 Z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 H 16 Z M 10,20 C 0,0 0,0 10,20 Z m 0,0 v 2 z m 0,0 C 0,0 0,0 10,20 Z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 2,0 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 0,0 C 0,0 0,0 16,20 Z m 0,0 v 2 z m 0,0 C 0,0 0,0 16,20 Z m -6,0 H 8 Z m -2,0 h 2 v 2 H 8 Z m 2,0 H 8 Z M 8,20 H 6 Z m -2,0 h 2 v 2 H 6 Z m 2,0 H 6 Z M 6,18 H 4 Z m -2,0 h 2 v 2 H 4 Z m 2,0 H 4 Z m 12,2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 2,-2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 H 18 Z M 10,10 H 8 Z m -2,0 h 2 v 2 H 8 Z m 2,0 H 8 Z m 0,2 H 8 Z m -2,0 h 2 v 2 H 8 Z m 2,0 H 8 Z m 0,2 H 8 Z m -2,0 h 2 v 2 H 8 Z m 2,0 H 8 Z m 0,2 H 8 Z m -2,0 h 2 v 2 H 8 Z m 2,0 H 8 Z m 0,2 H 8 Z m -2,0 h 2 v 2 H 8 Z m 2,0 H 8 Z m 6,-8 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 0,2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 0,2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 0,2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z m 0,2 h -2 z m -2,0 h 2 v 2 h -2 z m 2,0 h -2 z"
+      />
+    </svg>
+  );
+}
+
+export function Bread(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} {...props}>
+      <path
+        fill="currentColor"
+        d="m 1,8 h 2 v 2 H 1 Z m 20,2 h 2 v 2 h -2 z m 0,-2 h 2 v 2 h -2 z m 0,-2 h 2 V 8 H 21 Z M 1,6 H 3 V 8 H 1 Z m 2,10 h 2 v 2 H 3 Z m 0,-2 h 2 v 2 H 3 Z m 0,-2 h 2 v 2 H 3 Z m 16,0 h 2 v 2 H 19 Z M 1,10 h 2 v 2 H 1 Z m 18,10 v 2 z M 5,20 v 2 z m 0,0 h 2 v 2 H 5 Z m 2,0 h 2 v 2 H 7 Z m 2,0 h 2 v 2 H 9 Z m 2,0 h 2 v 2 h -2 z m 2,0 h 2 v 2 h -2 z m 2,0 h 2 v 2 h -2 z m 2,0 h 2 v 2 h -2 z m 2,-2 h 2 v 2 h -2 z m 0,-2 h 2 v 2 h -2 z m 0,-2 h 2 v 2 H 19 Z M 3,18 h 2 v 2 H 3 Z M 3,4 H 5 V 6 H 3 Z M 5,2 H 7 V 4 H 5 Z M 7,2 H 9 V 4 H 7 Z m 2,0 h 2 V 4 H 9 Z m 10,2 h 2 V 6 H 19 Z M 17,2 h 2 v 2 h -2 z m -2,0 h 2 v 2 h -2 z m -2,0 h 2 v 2 h -2 z m -2,2 h 2 v 2 h -2 zM 5,8 M 5,8 M 5,6 H 7 V 8 H 5 Z M 7,6 H 5 Z m 6,12 m 0,0 m 0,-2 h 2 v 2 h -2 z m 2,0 h -2 z m 0,0 m 0,0 m 0,-2 h 2 v 2 h -2 z m 2,0 H 15 Z M 1,8 h 2 v 2 H 1 Z m 20,2 h 2 v 2 h -2 z m 0,-2 h 2 v 2 h -2 z m 0,-2 h 2 V 8 H 21 Z M 1,6 H 3 V 8 H 1 Z m 2,10 h 2 v 2 H 3 Z m 0,-2 h 2 v 2 H 3 Z m 0,-2 h 2 v 2 H 3 Z m 16,0 h 2 v 2 H 19 Z M 1,10 h 2 v 2 H 1 Z m 18,10 v 2 z M 5,20 v 2 z m 0,0 h 2 v 2 H 5 Z m 2,0 h 2 v 2 H 7 Z m 2,0 h 2 v 2 H 9 Z m 2,0 h 2 v 2 h -2 z m 2,0 h 2 v 2 h -2 z m 2,0 h 2 v 2 h -2 z m 2,0 h 2 v 2 h -2 z m 2,-2 h 2 v 2 h -2 z m 0,-2 h 2 v 2 h -2 z m 0,-2 h 2 v 2 H 19 Z M 3,18 h 2 v 2 H 3 Z M 3,4 H 5 V 6 H 3 Z M 5,2 H 7 V 4 H 5 Z M 7,2 H 9 V 4 H 7 Z m 2,0 h 2 V 4 H 9 Z m 10,2 h 2 V 6 H 19 Z M 17,2 h 2 v 2 h -2 z m -2,0 h 2 v 2 h -2 z m -2,0 h 2 v 2 h -2 z m -2,2 h 2 v 2 h -2 z"
+      />
+    </svg>
+  );
+}
