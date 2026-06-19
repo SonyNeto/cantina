16afc9d209f57a636fd11880009a7824435df5c0
SonyNeto <sonyneto@gmail.com>
2026-06-19 00:32:58 -0300

    feature: 'feat: register responsible and students buttons

--- src/pages/registers/Registers.tsx
+++ src/pages/registers/Registers.tsx
@@ -1,10 +1,15 @@
-import type { FC } from 'react';
+import { useState, type FC } from 'react';
 import { RESPONSIBLES } from '../../constants/school/responsiblestemp';
 import { Link } from 'react-router';
 import ROUTES from '../../constants/routes';
 import { getResponsibleTotal } from '../../utils/selectors';
+import { Button } from '../../components/commons/Button';
+import { Check, Plus } from 'pixelarticons/react';
+import { X } from '../../assets/icons/MenuIcons';
 
 export const Registers: FC = () => {
+  const [isAdding, setIsAdding] = useState<boolean>(false);
+
   return (
     <div className="border-text m-6 flex h-fit flex-col overflow-hidden border-4">
       <div className="bg-tertiary flex w-full justify-center gap-2.5 px-6 py-4 text-xl">
@@ -22,10 +27,41 @@ export const Registers: FC = () => {
               className="border-text/40 text-text hover:bg-hover hover:text-text-hover relative z-50 grid w-full grid-cols-[minmax(0,1fr)_8ch] items-center gap-2.5 border-t-4 p-4 text-xl transition-all [&_svg]:size-10 [&_svg]:shrink-0"
             >
               <span>{responsible.name}</span>
-              <span className="text-right tabular-nums">{`R$ ${totalPerResponsible.toFixed(2)}`}</span>
+              <span className="text-right tabular-nums">{`R$${totalPerResponsible.toFixed(2)}`}</span>
             </Link>
           );
         })}
+
+        {isAdding ? (
+          <div className="bg-hover/30 border-text/40 z-50 flex w-full items-center justify-between gap-2.5 rounded-none border-t-4 p-4 text-xl font-medium [&_svg]:size-10 [&_svg]:shrink-0">
+            <div className="inline-flex items-center gap-2.5">
+              <input
+                id={`add-responsible-name`}
+                type="text"
+                placeholder="Nome do responsável"
+                className="border-text/40 w-[20ch] border-4 px-2"
+              />
+            </div>
+            <div className="flex flex-col gap-1.5">
+              <Button variant="primary" size="sm" onClick={() => setIsAdding(false)}>
+                <Check />
+              </Button>
+              <Button variant="primary" size="sm" onClick={() => setIsAdding(false)}>
+                <X />
+              </Button>
+            </div>
+          </div> 
+        ) : (
+          <Button
+            className="border-text/40 !h-full !w-full justify-center gap-2.5 rounded-none border-t-4 p-4 text-xl"
+            variant="ghost"
+            disabled={isAdding}
+            onClick={() => setIsAdding(true)}
+          >
+            <Plus />
+            Adicionar responsável
+          </Button>
+        )}
       </div>
     </div>
   );
--- src/pages/registers/ResponsibleDetails.tsx
+++ src/pages/registers/ResponsibleDetails.tsx
@@ -1,16 +1,19 @@
-import type { FC } from 'react';
+import { useState, type FC } from 'react';
 import { STUDENTS } from '../../constants/school/studentstemp';
 import { Link, useParams } from 'react-router';
 import ROUTES from '../../constants/routes';
-import { ArrowLeft } from 'pixelarticons/react';
+import { ArrowLeft, Check, Plus } from 'pixelarticons/react';
 import {
   getResponsibleNameById,
   getResponsibleTotal,
   getStudentTotal,
 } from '../../utils/selectors';
+import { Button } from '../../components/commons/Button';
+import { X } from '../../assets/icons/MenuIcons';
 
 export const ResponsibleDetails: FC = () => {
   const { responsibleId } = useParams();
+  const [isAdding, setIsAdding] = useState<boolean>(false);
 
   if (!responsibleId) {
     return <div>Respnsável não encontrado</div>;
@@ -40,15 +43,46 @@ export const ResponsibleDetails: FC = () => {
               className="border-text/40 text-text hover:bg-hover hover:text-text-hover relative z-50 grid w-full grid-cols-[minmax(0,1fr)_8ch] items-center gap-2.5 border-t-4 p-4 text-xl transition-all [&_svg]:size-10 [&_svg]:shrink-0"
             >
               <span>{student.name}</span>
-              <span className="text-right tabular-nums">{`R$ ${totalPerStudent.toFixed(2)}`}</span>
+              <span className="text-right tabular-nums">{`R$${totalPerStudent.toFixed(2)}`}</span>
             </Link>
           );
         })}
       </div>
 
+      {isAdding ? (
+        <div className="bg-hover/30 border-text/40 z-50 flex w-full items-center justify-between gap-2.5 rounded-none border-t-4 p-4 text-xl font-medium [&_svg]:size-10 [&_svg]:shrink-0">
+          <div className="inline-flex items-center gap-2.5">
+            <input
+              id={`add-responsible-name`}
+              type="text"
+              placeholder="Nome do aluno"
+              className="border-text/40 w-[20ch] border-4 px-2"
+            />
+          </div>
+          <div className="flex flex-col gap-1.5">
+            <Button variant="primary" size="sm" onClick={() => setIsAdding(false)}>
+              <Check />
+            </Button>
+            <Button variant="primary" size="sm" onClick={() => setIsAdding(false)}>
+              <X />
+            </Button>
+          </div>
+        </div> 
+      ) : (
+        <Button
+          className="border-text/40 !h-full !w-full justify-center gap-2.5 rounded-none border-t-4 p-4 text-xl"
+          variant="ghost"
+          disabled={isAdding}
+          onClick={() => setIsAdding(true)}
+        >
+          <Plus />
+          Adicionar aluno
+        </Button>
+      )}
+
       <div className="border-text/40 text-text relative grid w-full grid-cols-[minmax(0,1fr)_8ch] items-center gap-2.5 border-t-4 p-2 text-xl [&_svg]:size-10 [&_svg]:shrink-0">
         <span className="text-right">Total: </span>
-        <span className="text-right tabular-nums">{`R$ ${total.toFixed(2)}`}</span>
+        <span className="text-right tabular-nums">{`R$${total.toFixed(2)}`}</span>
       </div>
     </div>
   );
