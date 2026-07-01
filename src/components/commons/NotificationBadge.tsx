78803aff71be0c36158f0e0f6270d7e68883bbdb
SonyNeto <sonyneto@gmail.com>
2026-07-01 02:52:54 -0300

    style: format notification badge

--- src/components/commons/NotificationBadge.tsx
+++ src/components/commons/NotificationBadge.tsx
@@ -1,17 +1,16 @@
 type NotificationBadgeProps = {
   count: number;
-	max?: number;
-}
+  max?: number;
+};
 
-export const NotificationBadge = ({count, max = 9}: NotificationBadgeProps) => {
-	if (count <= 0) return null;
+export const NotificationBadge = ({ count, max = 9 }: NotificationBadgeProps) => {
+  if (count <= 0) return null;
 
-	const label = count > max ? `${max} +` : count;
+  const label = count > max ? `${max} +` : count;
 
-	return (
-		<span className="bg-red-600 text-white absolute -top-1 -right-1 flex min-h-7 min-w-7 items-center justify-center rounded-full px-1 text-base font-bold leading-none"
-		>
-			{label}
-		</span>
-	);
-}
+  return (
+    <span className="absolute -top-1 -right-1 flex min-h-7 min-w-7 items-center justify-center rounded-full bg-red-600 px-1 text-base leading-none font-bold text-white">
+      {label}
+    </span>
+  );
+};
