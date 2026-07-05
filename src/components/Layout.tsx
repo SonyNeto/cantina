78300c6bec83536562cd1ecb13bbc766c62d1761
SonyNeto <sonyneto@gmail.com>
2026-07-05 15:10:53 -0300

    fix: workspace fetach in layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,34 @@
-import type { FC } from 'react';
+import { useEffect, type FC } from 'react';
 import { Outlet } from 'react-router';
 import { NavBar } from './NavBar';
+import { useQuery } from '@tanstack/react-query';
+import { apiFetch } from '../utils/api';
+import { useWorkspaceStore, type Workspace } from '../stores/useWorkspaceStore';
+
+type WorkspacesResponse = {
+  workspaces: Workspace[];
+};
+
+const getWorkspaces = async (): Promise<WorkspacesResponse> => {
+  const res = await apiFetch('/workspaces');
+  return res.json();
+};
 
 export const Layout: FC = () => {
+  const setDefaultWorkspace = useWorkspaceStore.getState().setDefaultWorkspace;
+  
+  const { data: userWorkspaces } = useQuery({
+    queryKey: ['workspaces'],
+    queryFn: getWorkspaces,
+    select: (data) => data.workspaces,
+  });
+
+  useEffect(() => {
+    if (!userWorkspaces) return;
+
+    setDefaultWorkspace(userWorkspaces);
+  }, [setDefaultWorkspace, userWorkspaces]);
+
   return (
     <div className="flex h-screen w-screen flex-col overflow-auto">
       <NavBar />
